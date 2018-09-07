d5b47a1c86d2c0088ee4b266a7cdc08083fc57e3
Chris Jang <chris3jang@gmail.com>
2018-09-07 19:38:15 -0400

    heroku config

--- dist/app.js
+++ dist/app.js
@@ -2,7 +2,7 @@
 const Server = require('./server.js')
 const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
-const db = require('./config/db') || null;
+//const db = require('./config/db') || null;
 const port = (process.env.PORT || 8080)
 const app = Server.app()
 
@@ -19,6 +19,7 @@ app.use(passport.initialize());
 
 //let uri
 //if(db) uri = db.url
+//process.env.MONGOLAB_URI
 
 console.log("process.env.NODE_ENV", process.env.NODE_ENV)
 
@@ -27,7 +28,7 @@ if (process.env.NODE_ENV !== 'production') {
   const webpack = require('webpack')
   const webpackDevMiddleware = require('webpack-dev-middleware')
   const webpackHotMiddleware = require('webpack-hot-middleware')
-  const config = require('../webpack.config.js')
+  //const config = require('../webpack.config.js')
   //const config = require('../webpack.deployment.config.js')
   const compiler = webpack(config)
 
@@ -38,7 +39,7 @@ if (process.env.NODE_ENV !== 'production') {
   }))
 }
 
-MongoClient.connect(db.url, (err, database) => {
+MongoClient.connect(process.env.MONGOLAB_URI, (err, database) => {
 
   if (err) return console.log(err);
   app.use(bodyParser.urlencoded({ extended: true }));
