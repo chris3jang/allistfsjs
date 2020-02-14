89b52fc6c08152c24266f2574830cbabc24c4118
Chris Jang <chris3jang@gmail.com>
2020-02-13 22:02:01 -0500

    fix: converted navbar from class to function

--- app/navbar.js
+++ app/navbar.js
@@ -1,33 +1,20 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './css/navbar.css';
 import logo from './static/AllistLogo.jpeg'
 
-
-class NavBar extends React.Component {
-
-	handleTrashButtonClick = () => {
-		this.props.trashCheckedItemsFromNav();
-	}
-
-	handleLogoutButtonClick = () => {
-		this.props.logout();
-	}
-
-	render() {
-		return (
-			<div className={styles.topnav}>
-				<nav>
-					<img className={styles.logo} src={logo} />
-					<div className={styles.buttondiv}>
-						<button className={styles.button} onClick={this.handleTrashButtonClick.bind(this)}>Trash</button>
-						<button className={styles.button} onClick={this.handleLogoutButtonClick.bind(this)}>Logout</button>
-					</div>
-				</nav>
-			</div>
-		)
-	}
+const NavBar = ({trashCheckedItemsFromNav, logout}) => {
+	return (
+		<div className={styles.topnav}>
+			<nav>
+				<img className={styles.logo} src={logo} />
+				<div className={styles.buttondiv}>
+					<button className={styles.button} onClick={() => trashCheckedItemsFromNav()}>Trash</button>
+					<button className={styles.button} onClick={() => logout()}>Logout</button>
+				</div>
+			</nav>
+		</div>
+	)
 }
 
 export default NavBar;
