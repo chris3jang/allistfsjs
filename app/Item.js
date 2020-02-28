1e6f416c69d0ffcbde5a8818fcf538d7b5b0cae1
Chris Jang <chris3jang@gmail.com>
2020-02-28 00:44:40 -0500

    fix: item migration to jss

--- app/Item.js
+++ app/Item.js
@@ -1,13 +1,27 @@
 import React, {useState, useEffect, useRef} from 'react';
-import ContentEditable from 'react-contenteditable'
-import styles from './css/item.css';
 
 import Bullet from './Bullet.js'
 import EditableText from './EditableText.js'
+import {createUseStyles} from 'react-jss'
+
+const useStyles = createUseStyles({
+	indentation: {
+		paddingLeft: item => item.indentLevel * 20
+	},
+	unfinished: {
+		marginTop: '10px'
+	},
+	complete: {
+	  marginTop: '10px',
+	  textDecoration: 'line-through',
+	  opacity: '.2'
+	}
+})
 
 const Item = ({item, checked, handleAction}) => {
+	const classes = useStyles(item);
 	return (
-		<div style={{paddingLeft: 20 * item.indentLevel}} className={checked ? styles.itemCompleted : styles.item}>
+		<div className={`${classes.indentation} ${checked ? classes.complete : classes.unfinished}`}>
 			<Bullet orderNumber={item.orderNumber} decollapsed={item.decollapsed} checked={item.checked} handleAction={handleAction}/>
 			<EditableText id={item._id} orderNumber={item.orderNumber} itemTitle={item.itemTitle} checked={item.checked} handleAction={handleAction}/>
 		</div>
